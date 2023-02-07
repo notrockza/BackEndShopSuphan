6d08fc51f7d7960c7a92e812382f4223965d5366
LAPTOP-TMIS5LGJ\Asus <notrockza01@gmail.com>
2023-02-07 20:47:54 +0700

    Andnew

---
+++ interfaces/ICommunityGroupService.cs
@@ -0,0 +1,14 @@
+﻿using ShopSuphan.Models;
+
+namespace ShopSuphan.interfaces
+{
+    public interface ICommunityGroupService
+    {
+        Task<IEnumerable<CommunityGroup>> GetAll();
+
+        Task<CommunityGroup> GetByID(int id);
+        Task Create(CommunityGroup แommunityGroup);
+        Task Update(CommunityGroup แommunityGroup);
+        Task Delete(CommunityGroup แommunityGroup);
+    }
+}
